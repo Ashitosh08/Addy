d2d30321d5002638b5b1a5d4d7ce66fabc3d0ee2
Ashitosh07 <Ashitosh.K@raasoft.com>
2023-08-26 19:20:48 +0530

    commit changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -24,21 +24,29 @@ const Header = () => {
       whileInView="show"
       className={`bg-primary paddings ${css.wrapper}`}
       viewport={{ once: true, amount: 0.25 }}
-      style={{boxShadow: headerShadow}}
+      style={{ boxShadow: headerShadow }}
     >
       <div className={`innerWidth ${css.container} flexCenter`}>
-        <div className={css.name}>Binjan</div>
+        <div className={css.name}>AddynMolly</div>
         <ul
           className={`flexCenter ${css.menu}`}
           ref={menuRef}
           style={getMenuStyles(menuOpened)}
         >
-          <li><a href="#experties">Services</a></li>
-          <li><a href="#work">Experience</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
-          <li><a href="#people">Testimonials</a></li>
+          <li>
+            <a href="#experties">Home</a>
+          </li>
+          <li>
+            <a href="#work">Products</a>
+          </li>
+          <li>
+            <a href="#portfolio">Services</a>
+          </li>
+          <li>
+            <a href="#people">About us</a>
+          </li>
           <li className={`flexCenter ${css.phone}`}>
-            <p>+001 (313) 345 678</p>
+            <p></p>
             <BiPhoneCall size={"40px"} />
           </li>
         </ul>
